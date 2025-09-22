6ae7d16276c6f55e2667aac57cb948567e569c3a
kyawswarlynn <132581651+kyawswarlynn22@users.noreply.github.com>
2025-09-22 12:14:28 +0630

    Update favicon and metadata, simplify image styling
    Replaces the favicon.ico file and updates the metadata in layout.js to specify icon paths for various devices. Also simplifies the About section image by removing transition and hover effects.

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,11 @@ export const metadata = {
   title: "Portfolio of Kyaw Swar Lynn - Software Developer",
   description:
     "This is the portfolio of Kyaw Swar Lynn. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+  icons: {
+    icon: '/favicon.ico', // Path to your favicon in public directory
+    shortcut: '/favicon.ico',
+    apple: '/apple-icon.png', // Optional: for Apple devices
+  },
 };
 
 export default function RootLayout({ children }) {
